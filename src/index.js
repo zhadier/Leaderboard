a66377f0b6c5b8d34f14d7b9d1a89af1aabc7368
Zeeshan Haider <zhadier39@gmail.com>
2022-01-26 00:12:38 +0500

    add initial functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import _ from 'lodash';
 import './style.css';
+import Collection from './modules/collection.js';
+import User from './modules/user.js';
 
-function component() {
-  const element = document.createElement('div');
+const inputName = document.getElementById('name');
+const inputScore = document.getElementById('score');
+const submitBtn = document.querySelector('#submit-button');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
+const coll = new Collection();
+if (localStorage.getItem('Scores')) {
+  const localBooks = JSON.parse(localStorage.getItem('Scores'));
+  localBooks.scoreList.forEach((element) => {
+    coll.add(new User(element.name, element.score));
+  });
 }
 
-document.body.appendChild(component());
+submitBtn.addEventListener('click', () => {
+  coll.add(new User(inputName.value, inputScore.value));
+  inputScore.value = '';
+  inputName.value = '';
+});
