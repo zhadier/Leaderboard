247fe41e184f7428c94de6428416b31cf5348647
Zeeshan Haider <zhadier39@gmail.com>
2022-01-28 17:39:35 +0500

    change bg

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const inputScore = document.getElementById('score');
 const submitBtn = document.querySelector('#submit-button');
 const refreshBtn = document.querySelector('#refresh-button');
 const logo = document.querySelector('header a');
+const getMessage = document.querySelector('.add-score__heading');
+const body = document.querySelector('body');
+let i = 0;
 
 submitBtn.addEventListener('click', () => {
   util.addToLB({ user: inputName.value, score: inputScore.value });
@@ -22,5 +25,14 @@ window.addEventListener('load', () => {
 });
 
 logo.addEventListener('click', () => {
+  body.classList.toggle(`b${i}`);
+  i += 1;
+  if (i > 5) {
+    i = 0;
+  }
+  body.classList.toggle(`b${i}`);
+});
+
+getMessage.addEventListener('click', () => {
   util.popUp();
 });
